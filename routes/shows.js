2304495136ea6bcb05f56774d47d2f5653cf1046
Florian Sylvain <floriansylvainpro@gmail.com>
2022-11-10 19:01:48 +0100

    🎨 - Improved prisma funcs usage

--- routes/shows.js
+++ routes/shows.js
@@ -5,38 +5,38 @@ import createHttpError from 'http-errors'
 const prisma = new PrismaClient()
 export const showsRouter = express.Router()
 
-showsRouter.get('/', async function(req, res, next) {
+showsRouter.get('/', async function (req, res, next) {
 	const shows = await prisma.serie.findMany()
 
-	res.send({shows})
+	res.send({ shows })
 })
 
-showsRouter.get('/:id', function(req, res, next) {
+showsRouter.get('/:id', function (req, res, next) {
 	prisma.serie.findUniqueOrThrow({
 		where: {
-			title: {
-				equals: req.params.id
-			}
-		}})
-	.then(show => res.send(show))
-	.catch(error => {
-		if (error instanceof Prisma.NotFoundError) {
-			next(createHttpError(404, 'Show unknown.'))
+			title: req.params.id
 		}
 	})
+		.then(show => res.send({ show }))
+		.catch(error => {
+			if (error instanceof Prisma.NotFoundError) {
+				next(createHttpError(404, 'Show not found.'))
+			}
+		})
 })
 
-showsRouter.post('/', async function (req, res, next) {
+showsRouter.post('/', function (req, res, next) {
 	prisma.serie.create({
 		data: {
 			title: req.body.title,
 			date: req.body.date,
 			description: req.body.description,
 			thumbmailURL: req.body.thumbmailURL
-		}})
-	.then(() => res.send({message: 'Show posted!'}))
-	.catch(() => {
-		next(createHttpError(400, 'Check your show model/content.'))
+		}
 	})
+		.then(() => res.send({ message: 'Show posted!' }))
+		.catch(() => {
+			next(createHttpError(400, 'Check your show model/content.'))
+		})
 })
 
